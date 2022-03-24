2cb6eb3f0edb36eb0535c36622206d7a289fa22f
Ocholla-T <tonnyocholla8@gmail.com>
2022-03-24 22:43:10 +0300

    ADD: added conditional rendering for menu and styles for mobile and tab screens

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,70 @@
+import { useRef, useState } from 'react';
 import logo from '../images/logo.svg';
 import '../styles/components/_navbar.scss';
 
 function Navbar() {
+  const [isShown, setIsShown] = useState(false);
+
+  function toggleMenu() {
+    setIsShown((previousShown) => !previousShown);
+  }
+
   return (
     <header>
-      <nav className="nav">
+      <nav className="nav flex flex-ai-c flex-jc-sb">
         <img className="nav__logo" src={logo} alt="logo" />
-        <div className="nav__links">
-          <ol>
+        <div className="nav__links flex flex-ai-c hide-for-mobile">
+          <ol className="flex">
             <li>
-              <span>01.</span>about
+              <a href="">
+                <span>01.</span>About
+              </a>
             </li>
             <li>
-              <span>02.</span>projects
+              <a href="">
+                <span>02.</span>Projects
+              </a>
             </li>
             <li>
-              <span>03.</span>contact
+              <a href="">
+                <span>03.</span>Contact
+              </a>
             </li>
           </ol>
           <a className="nav__links__button"> Resume</a>
         </div>
+
+        {/* mobile and tablet screen markup */}
+
+        <div className="nav__menu hide-for-desktop" onClick={toggleMenu} />
+        {isShown && (
+          <aside className="nav__mobileLinks flex flex-ai-c flex-jc-c hide-for-desktop">
+            <ol className="flex">
+              <li>
+                <a href="">
+                  <span>01.</span>
+                  <br />
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  <span>02.</span>
+                  <br />
+                  Projects
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  <span>03.</span>
+                  <br />
+                  Contact
+                </a>
+              </li>
+            </ol>
+            <a className="nav__mobileLinks__button"> Resume</a>
+          </aside>
+        )}
       </nav>
     </header>
   );
