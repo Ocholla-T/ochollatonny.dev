1c7894bab0a0bc197c0353ea757e11c4539fc839
Ocholla-T <tonnyocholla8@gmail.com>
2022-03-24 17:53:58 +0300

    ADD: imported navbar component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,19 @@
+import Navbar from '../components/Navbar';
+
+function Home() {
+  return (
+    <main>
+      <Navbar />
+      {/* <h4>Hi, my name is</h4>
+      <h1>Ocholla Tonny.</h1>
+      <h2>You could say I conjure things for the web</h2>
+      <p>
+        I'm a frontend web developer specializing in building digital experiences for the web.
+        Currently I'm focused on learning and becoming amazing at creating accessible, user-centred
+        products.
+      </p> */}
+    </main>
+  );
+}
+
+export default Home;
