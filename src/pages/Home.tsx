a4284522498c64b8e6dced1b9bbdf74124fa773e
Ocholla-T <tonnyocholla8@gmail.com>
2022-03-28 20:41:07 +0300

    ADD: added hero section markup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,27 @@
 import Navbar from '../components/Navbar';
 import '../styles/pages/_home.scss';
+import downloadIcon from '../images/download.svg';
 
-function Home() {
+function Home(): JSX.Element {
   return (
     <main>
       <Navbar />
-      {/* <h4>Hi, my name is</h4>
-      <h1>Ocholla Tonny.</h1>
-      <h2>You could say I conjure things for the web</h2>
-      <p>
-        I'm a frontend web developer specializing in building digital experiences for the web.
-        Currently I'm focused on learning and becoming amazing at creating accessible, user-centred
-        products.
-      </p> */}
+      <section className="hero">
+        <h4>Hi there &#128075;, I'm</h4>
+        <h1>Ocholla Tonny.</h1>
+        <p>
+          <span>Frontend web developer</span> specializing in delivering digital experiences for the
+          web.
+        </p>
+        <p>
+          I'm always focused on continuous growth when learning & creating accessible, user-centred
+          products.
+        </p>
+        <a href="" className="hero__button button flex flex-ai-c flex-jc-c">
+          <img className="hero__button__icon " src={downloadIcon} alt="download icon" />
+          Resume
+        </a>
+      </section>
     </main>
   );
 }
