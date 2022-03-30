761a8d025449120fa73975cb61e988f8028524d4
Ocholla-T <tonnyocholla8@gmail.com>
2022-03-30 23:13:56 +0300

    Added blur effect on opening mobile menu

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,22 @@
 import Navbar from '../layouts/Navbar';
 import '../styles/pages/_home.scss';
-import downloadIcon from '../images/download.svg';
+import ArrowIcon from '../images/arrow-outline.svg';
+import ScrollIcon from '../images/arrow-down-outline.svg';
+import SocialIcons from '../layouts/SocialIcons';
+import Resume from '../images/Ocholla Tonny Resume.pdf';
+import { useState } from 'react';
 
 function Home(): JSX.Element {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false);
+
+  const getIsMobileMenuOpenState: Function = (stateOfMobileMenu: boolean): void => {
+    setIsMobileMenuOpen(stateOfMobileMenu);
+  };
+
   return (
-    <main>
-      <Navbar />
-      <section className="hero">
+    <main className="flex flex-fd-c ">
+      <Navbar getIsMobileMenuOpenState={getIsMobileMenuOpenState} />
+      <section className={`hero flex flex-fd-c ${isMobileMenuOpen && 'blur'} `}>
         <h4>Hi there &#128075;, I'm</h4>
         <h1>Ocholla Tonny.</h1>
         <p>
@@ -17,11 +27,18 @@ function Home(): JSX.Element {
           I'm always focused on continuous growth when learning & creating accessible, user-centred
           products.
         </p>
-        <a href="" className="hero__button button flex flex-ai-c flex-jc-c">
-          <img className="hero__button__icon " src={downloadIcon} alt="download icon" />
+        <a
+          href={Resume}
+          className="hero__button button flex flex-ai-c flex-jc-c"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Resume
+          <img className="hero__button__icon " src={ArrowIcon} alt="next" />
         </a>
       </section>
+      <SocialIcons />
+      <img className="scroll-icon hide-for-mobile" src={ScrollIcon} alt="scroll" />
     </main>
   );
 }
