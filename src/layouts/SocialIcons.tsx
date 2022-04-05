7e68f0549c0532a74e9bd4dd551676f2d440f22c
Ocholla-T <tonnyocholla8@gmail.com>
2022-04-05 17:11:47 +0300

    CHANGE: update icons to render as components

--- src/layouts/SocialIcons.tsx
+++ src/layouts/SocialIcons.tsx
@@ -1,15 +1,15 @@
 import '../styles/layouts/_social-icons.scss';
-import linkedInIcon from '../images/logo-linkedin.svg';
-import githubIcon from '../images/logo-github.svg';
+import { ReactComponent as LinkedInIcon } from '../images/logo-linkedin.svg';
+import { ReactComponent as GithubIcon } from '../images/logo-github.svg';
 
 function SocialIcons() {
   return (
     <aside className="social container flex flex-fd-c flex-ai-c hide-for-mobile">
       <a href="https://github.com/Ocholla-T" target="_blank" rel="noopener noreferrer">
-        <img className="social__icon" src={githubIcon} alt="github" />
+        <GithubIcon className="social__icon" />
       </a>
       <a href="https://www.linkedin.com/in/ochollatonny" target="_blank" rel="noopener noreferrer">
-        <img className="social__icon" src={linkedInIcon} alt="linkedIn" />
+        <LinkedInIcon className="social__icon" />
       </a>
       <div className="social__line" />
     </aside>
