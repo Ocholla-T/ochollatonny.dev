e4831491fbf1bf61fd82a9fb9986202a5841fc50
Ocholla-T <tonnyocholla8@gmail.com>
2022-04-05 17:11:18 +0300

    CHANGE: moved hero section to its own layout component

---
+++ src/layouts/HeroSection.tsx
@@ -0,0 +1,37 @@
+import { ReactComponent as ArrowIcon } from '../images/arrow-outline.svg';
+import { ReactComponent as ScrollIcon } from '../images/arrow-down-outline.svg';
+import Resume from '../images/Ocholla Tonny Resume.pdf';
+import '../styles/layouts/_hero-section.scss';
+
+interface Props {
+  isMobileMenuOpen: boolean;
+}
+
+function HeroSection({ isMobileMenuOpen }: Props): JSX.Element {
+  return (
+    <section className={`hero flex flex-fd-c ${isMobileMenuOpen && 'blur'} `}>
+      <h4>Hi there &#128075;, I'm</h4>
+      <h1>Ocholla Tonny.</h1>
+      <p>
+        <span>Frontend web developer</span> specializing in delivering digital experiences for the
+        web.
+      </p>
+      <p>
+        I'm always focused on continuous growth when learning & creating accessible, user-centred
+        products.
+      </p>
+      <a
+        href={Resume}
+        className="hero__button button flex flex-ai-c flex-jc-c"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Resume
+        <ArrowIcon className="hero__button__icon" />
+      </a>
+      <ScrollIcon className="scroll-icon hide-for-mobile" />
+    </section>
+  );
+}
+
+export default HeroSection;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,9 @@
 import Navbar from '../layouts/Navbar';
 import '../styles/pages/_home.scss';
-import ArrowIcon from '../images/arrow-outline.svg';
-import ScrollIcon from '../images/arrow-down-outline.svg';
 import SocialIcons from '../layouts/SocialIcons';
-import Resume from '../images/Ocholla Tonny Resume.pdf';
 import { useState } from 'react';
 import ProjectCard from '../components/ProjectCard';
+import HeroSection from '../layouts/HeroSection';
 
 function Home(): JSX.Element {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false);
@@ -13,33 +11,11 @@ function Home(): JSX.Element {
   const getIsMobileMenuOpenState: Function = (stateOfMobileMenu: boolean): void => {
     setIsMobileMenuOpen(stateOfMobileMenu);
   };
-
   return (
     <main className="flex flex-fd-c ">
       <Navbar getIsMobileMenuOpenState={getIsMobileMenuOpenState} />
-      <section className={`hero flex flex-fd-c ${isMobileMenuOpen && 'blur'} `}>
-        <h4>Hi there &#128075;, I'm</h4>
-        <h1>Ocholla Tonny.</h1>
-        <p>
-          <span>Frontend web developer</span> specializing in delivering digital experiences for the
-          web.
-        </p>
-        <p>
-          I'm always focused on continuous growth when learning & creating accessible, user-centred
-          products.
-        </p>
-        <a
-          href={Resume}
-          className="hero__button button flex flex-ai-c flex-jc-c"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Resume
-          <img className="hero__button__icon " src={ArrowIcon} alt="next" />
-        </a>
-      </section>
+      <HeroSection isMobileMenuOpen={isMobileMenuOpen} />
       <SocialIcons />
-      <img className="scroll-icon hide-for-mobile" src={ScrollIcon} alt="scroll" />
       <ProjectCard />
     </main>
   );
