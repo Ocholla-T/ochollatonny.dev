fc0e530b83f7f804fac839e0b5e502fb5e915a5f
Ocholla-T <tonnyocholla8@gmail.com>
2022-04-12 19:38:25 +0300

    ADD: created a card component for all projects

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,5 +1,37 @@
-const ProjectCard = (): JSX.Element => {
-  return <div className="card container">hello</div>;
+import '../styles/components/_project-card.scss';
+import { ReactComponent as FlagIcon } from '../images/flag-outline.svg';
+
+interface Props {
+  projectTitle: string;
+  projectLink: string;
+  projectDescription: string;
+  techUsed: string[];
+}
+
+const ProjectCard = ({
+  projectTitle,
+  projectDescription,
+  techUsed,
+  projectLink,
+}: Props): JSX.Element => {
+  return (
+    <section id="projects" className="projects container flex flex-ai-c">
+      <h1 className="projects__title">Projects</h1>
+
+      <a href={projectLink}>
+        <article className="projects__card flex flex-jc-sb">
+          <div className="projects__card__text">
+            <p className="projects__card__text--tech"> {techUsed.join(' ')}</p>
+
+            <h2 className="projects__card__text--title">{projectTitle}</h2>
+            <p className="projects__card__text--description">{projectDescription}</p>
+          </div>
+
+          <FlagIcon className="projects__card__icon" />
+        </article>
+      </a>
+    </section>
+  );
 };
 
 export default ProjectCard;
