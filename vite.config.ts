0a7580db12f0562c23a22e7aac55c74bf21d1ca0
Ocholla-T <tonnyocholla8@gmail.com>
2022-04-13 17:06:36 +0300

    REFACTOR: Add path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import * as path from 'path';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@layouts': path.resolve(__dirname, './src/components/layouts'),
+      '@images': path.resolve(__dirname, './src/images'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+    },
+  },
   plugins: [
     svgr({
       defaultExport: false,
