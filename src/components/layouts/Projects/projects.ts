77a1d8ab00e3d45b28375212ecd698f94e4059b7
Ocholla-T <tonnyocholla8@gmail.com>
2022-04-15 22:18:55 +0300

    ADD: objects representing projects done

---
+++ src/components/layouts/Projects/projects.ts
@@ -0,0 +1,26 @@
+export default [
+  {
+    projectTitle: 'Country Info',
+    projectLink: 'https://countryinfo-ochollat.netlify.app/',
+    projectDescription:
+      'Integrates with the REST Countries V2 API to pull data and display it like in the provided designs',
+    techUsed: ['Vuex', 'VueJS'],
+    imageSource: '/src/images/flag-outline.svg',
+  },
+  {
+    projectTitle: 'Shortly',
+    projectLink: 'https://shortly-ochollat.netlify.app/',
+    projectDescription:
+      'Integrates with the shrtcode API to create shortened URLs and display them like in the designs. Worked with browser storage to persist data.',
+    techUsed: ['VueJS'],
+    imageSource: '/src/images/link-outline.svg',
+  },
+  {
+    projectTitle: 'ochollatonny.dev',
+    projectLink: 'https://countryinfo-ochollat.netlify.app/',
+    projectDescription:
+      'Frontend Mentor challenge that integrates with the REST Countries V2 API to pull data and display it like in the provided designs',
+    techUsed: ['ReactJS', 'Typescript'],
+    imageSource: '/src/images/code-slash-outline.svg',
+  },
+];
