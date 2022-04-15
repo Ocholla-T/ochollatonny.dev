353d8f89d141e0b8392f3d8320658a4af4da8e66
Ocholla-T <tonnyocholla8@gmail.com>
2022-04-15 22:23:20 +0300

    REFACTOR: changed projects section

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 import Navbar from '@layouts/NavBar';
 import SocialIcons from '@layouts/SocialIcons';
 import HeroSection from '@layouts/HeroSection';
-import ProjectCard from '@components/ProjectCard';
+import Projects from '@layouts/Projects';
 
 /* Styling */
 import './styles.scss';
@@ -16,18 +16,13 @@ function Home(): JSX.Element {
   const getIsMobileMenuOpenState: Function = (stateOfMobileMenu: boolean): void => {
     setIsMobileMenuOpen(stateOfMobileMenu);
   };
+
   return (
     <main className="flex flex-fd-c ">
       <Navbar getIsMobileMenuOpenState={getIsMobileMenuOpenState} />
       <HeroSection isMobileMenuOpen={isMobileMenuOpen} />
       <SocialIcons />
-      <ProjectCard
-        techUsed={['Vuex', 'VueJS']}
-        projectTitle="Country Info"
-        projectLink="https://countryinfo-ochollat.netlify.app/"
-        projectDescription="Frontend Mentor challenge that integrates with the REST Countries V2 API to pull data
-              and display it like in the provided designs"
-      />
+      <Projects />
     </main>
   );
 }
