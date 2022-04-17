75b15e10d977c4738b8aafc44045e5de4f4ca025
Ocholla-T <tonnyocholla8@gmail.com>
2022-04-17 23:11:14 +0300

    REFACTOR: changed how the projects object is passed down to the card component and its interface declaration

--- src/components/layouts/Projects/Card/index.tsx
+++ src/components/layouts/Projects/Card/index.tsx
@@ -1,37 +1,49 @@
-/* Images */
+/* Dependencies */
 import { ReactNode } from 'react';
+
+/* Images */
+import { ReactComponent as OpenLink } from '@images/open-outline.svg';
+import { ReactComponent as GithubIcon } from '@images/logo-github.svg';
 
 /* Styling */
 import './styles.scss';
 
 interface Props {
-  projectTitle: string;
-  projectLink: string;
-  projectDescription: string;
-  techUsed: string[];
   children: ReactNode;
+  project: {
+    githubLink: string;
+    imageSource: string;
+    projectDescription: string;
+    projectLink: string;
+    projectTitle: string;
+    techUsed: string[];
+  };
 }
 
-const Card = ({
-  projectTitle,
-  projectDescription,
-  techUsed,
-  projectLink,
-  children,
-}: Props): JSX.Element => {
-  return (
-    <a href={projectLink}>
-      <article className="card flex flex-jc-sb">
-        <div className="card__text">
-          <p className="card__text--tech"> {techUsed.join(' ')}</p>
+function Card({ children, project }: Props): JSX.Element {
+  const goToProject = (): void => {
+    window.open(`${project.projectLink}`, '_blank');
+  };
 
-          <h2 className="card__text--title">{projectTitle}</h2>
-          <p className="card__text--description">{projectDescription}</p>
+  return (
+    <article className="card flex flex-jc-sb" onClick={goToProject}>
+      <div className="card__text">
+        <p className="card__text--tech"> {project.techUsed.join(' ')}</p>
+
+        <h2 className="card__text--title">{project.projectTitle}</h2>
+        <p className="card__text--description">{project.projectDescription}</p>
+        <div className="card__links flex">
+          <a href={project.projectLink} target="_blank" rel="noopener noreferrer">
+            <OpenLink className="card__links--icon card__links--icon-open" />
+          </a>
+          <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
+            <GithubIcon className="card__links--icon card__links--icon-github" />
+          </a>
         </div>
-        {children}
-      </article>
-    </a>
+      </div>
+      {children}
+    </article>
   );
-};
+}
 
 export default Card;
--- src/components/layouts/Projects/index.tsx
+++ src/components/layouts/Projects/index.tsx
@@ -4,41 +4,36 @@ import projects from './projects';
 /* Components*/
 import Card from './Card';
 
-import { ReactComponent as FlagIcon } from '@images/flag-outline.svg';
-
 /* Styles*/
 import './styles.scss';
-import { ReactNode } from 'react';
 
 interface Projects {
-  projectTitle: string;
-  projectLink: string;
-  projectDescription: string;
-  techUsed: string[];
+  githubLink: string;
   imageSource: string;
+  projectDescription: string;
+  projectLink: string;
+  projectTitle: string;
+  techUsed: string[];
+}
+interface ProjectsProps {
+  isMobileMenuOpen: boolean;
 }
 
-function Projects() {
+function Projects({ isMobileMenuOpen }: ProjectsProps): JSX.Element {
   return (
-    <section id="projects" className="projects container flex flex-ai-c">
-      <h1 className="projects__title">Projects</h1>
-      <div>
-        {projects.map(
-          (
-            { projectTitle, projectLink, projectDescription, techUsed, imageSource }: Projects,
-            index: number,
-          ) => (
-            <Card
-              key={index}
-              projectTitle={projectTitle}
-              projectLink={projectLink}
-              projectDescription={projectDescription}
-              techUsed={techUsed}
-            >
-              <img src={imageSource} className="card__icon" alt="" />
+    <section id="projects" className={`projects container flex ${isMobileMenuOpen && 'blur'}`}>
+      <h3 className="projects__subtitle">
+        <span>02.</span> Let's see what I can do...
+      </h3>
+      <div className="flex flex-ai-c">
+        <h1 className="projects__title hide-for-mobile">Projects</h1>
+        <div>
+          {projects.map((project: Projects, index: number) => (
+            <Card key={index} project={project}>
+              <img src={project.imageSource} className="card__icon hide-for-mobile" alt="" />
             </Card>
-          ),
-        )}
+          ))}
+        </div>
       </div>
     </section>
   );
