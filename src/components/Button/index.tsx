46e45573a451f3056ff78514b920456e155d0343
Ocholla-T <tonnyocholla8@gmail.com>
2022-04-17 23:00:33 +0300

    init.

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,8 @@
+/** Styles */
+import './styles.scss';
+
+function Button(): JSX.Element {
+  return <p>hello</p>;
+}
+
+export default Button;
---
+++ src/components/layouts/Contacts/index.tsx
@@ -0,0 +1,12 @@
+/*Styles */
+import './styles.scss';
+
+function Contacts(): JSX.Element {
+  return (
+    <section id="contacts" className="contacts container flex flex-jc-c">
+      <h3 className="contacts__title">03. What's next?</h3>
+    </section>
+  );
+}
+
+export default Contacts;
