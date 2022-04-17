cf324d80077883663d10bbe231d511310b97bc04
Ocholla-T <tonnyocholla8@gmail.com>
2022-04-17 23:06:28 +0300

    ADD: added links to github, project and images

--- src/components/layouts/Projects/projects.ts
+++ src/components/layouts/Projects/projects.ts
@@ -1,26 +1,29 @@
 export default [
   {
-    projectTitle: 'Country Info',
-    projectLink: 'https://countryinfo-ochollat.netlify.app/',
+    githubLink: 'https://github.com/Ocholla-T/country',
+    imageSource: '/src/images/flag-outline.svg',
     projectDescription:
       'Integrates with the REST Countries V2 API to pull data and display it like in the provided designs',
+    projectLink: 'https://countryinfo-ochollat.netlify.app/',
+    projectTitle: 'Country Info',
     techUsed: ['Vuex', 'VueJS'],
-    imageSource: '/src/images/flag-outline.svg',
   },
   {
-    projectTitle: 'Shortly',
-    projectLink: 'https://shortly-ochollat.netlify.app/',
+    githubLink: 'https://github.com/Ocholla-T/shortly',
+    imageSource: '/src/images/link-outline.svg',
     projectDescription:
       'Integrates with the shrtcode API to create shortened URLs and display them like in the designs. Worked with browser storage to persist data.',
+    projectLink: 'https://shortly-ochollat.netlify.app/',
+    projectTitle: 'Shortly',
     techUsed: ['VueJS'],
-    imageSource: '/src/images/link-outline.svg',
   },
   {
-    projectTitle: 'ochollatonny.dev',
-    projectLink: 'https://countryinfo-ochollat.netlify.app/',
+    githubLink: 'https://github.com/Ocholla-T/ochollatonny.dev',
+    imageSource: '/src/images/code-slash-outline.svg',
     projectDescription:
       'Frontend Mentor challenge that integrates with the REST Countries V2 API to pull data and display it like in the provided designs',
+    projectLink: 'https://countryinfo-ochollat.netlify.app/',
+    projectTitle: 'ochollatonny.dev',
     techUsed: ['ReactJS', 'Typescript'],
-    imageSource: '/src/images/code-slash-outline.svg',
   },
 ];
