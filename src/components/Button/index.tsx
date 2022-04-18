b69863c9b8fd3b8c7305be78f5d7780378c61ff9
Ocholla-T <tonnyocholla8@gmail.com>
2022-04-18 16:49:27 +0300

    ADD: button reusable component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,24 @@
 /** Styles */
+import { ReactNode } from 'react';
 import './styles.scss';
 
-function Button(): JSX.Element {
-  return <p>hello</p>;
+interface ButtonProps {
+  buttonLink: string;
+  children: ReactNode;
+  className?: string;
+}
+
+function Button({ buttonLink, className, children }: ButtonProps): JSX.Element {
+  return (
+    <a
+      href={buttonLink}
+      className={`button flex flex-ai-c flex-jc-c ${className}`}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {children}
+    </a>
+  );
 }
 
 export default Button;
