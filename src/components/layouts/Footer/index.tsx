59051a4312d3cbd62d05d073718279fe750e3e5e
Ocholla-T <tonnyocholla8@gmail.com>
2022-04-19 21:23:54 +0300

    ADD: attribution to creator of website

---
+++ src/components/layouts/Footer/index.tsx
@@ -0,0 +1,13 @@
+/**Dependencies */
+import { FC } from 'react';
+
+/**Styles */
+import './styles.scss';
+
+type Props = {};
+
+const Footer: FC<Props> = (props: Props) => {
+  return <footer className="footer flex flex-jc-c">Built by Ocholla Tonny</footer>;
+};
+
+export default Footer;
