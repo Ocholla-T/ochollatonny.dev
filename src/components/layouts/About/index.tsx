5bbdfda536dc7d90b7d93802b0dc98500cd1e95e
Ocholla-T <tonnyocholla8@gmail.com>
2022-04-20 08:35:01 +0300

    init

---
+++ src/components/layouts/About/index.tsx
@@ -0,0 +1,14 @@
+/**Styles */
+import './styles.scss';
+
+type Props = {};
+
+const About = (props: Props) => {
+  return (
+    <section className="about container flex">
+      <h3 className="about__title">01. About Me</h3>
+    </section>
+  );
+};
+
+export default About;
