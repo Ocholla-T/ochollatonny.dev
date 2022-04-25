1664dbbe8af1d1caca66ae7b48c206ad5e4e65b7
Ocholla-T <tonnyocholla8@gmail.com>
2022-04-25 08:29:06 +0300

    REFACTOR

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,23 +1,23 @@
 /* Dependencies */
-import { useState } from 'react';
+import { useState } from 'react'
 
 /* Components */
-import Contacts from '@layouts/Contacts';
-import Footer from '@layouts/Footer';
-import HeroSection from '@layouts/HeroSection';
-import Navbar from '@layouts/NavBar';
-import Projects from '@layouts/Projects';
-import SocialIcons from '@layouts/SocialIcons';
+import Contacts from '@layouts/Contacts'
+import Footer from '@layouts/Footer'
+import HeroSection from '@layouts/HeroSection'
+import Navbar from '@layouts/NavBar'
+import Projects from '@layouts/Projects'
+import SocialIcons from '@layouts/SocialIcons'
 
 /* Styling */
-import './styles.scss';
+import './styles.scss'
 
 function Home(): JSX.Element {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false)
 
   const getIsMobileMenuOpenState: Function = (stateOfMobileMenu: boolean): void => {
-    setIsMobileMenuOpen(stateOfMobileMenu);
-  };
+    setIsMobileMenuOpen(stateOfMobileMenu)
+  }
 
   return (
     <main className="flex flex-fd-c ">
@@ -28,7 +28,7 @@ function Home(): JSX.Element {
       <Contacts />
       <Footer />
     </main>
-  );
+  )
 }
 
-export default Home;
+export default Home
