6f2fa4f5e55bfe4b76dd392d7bd9bda725d20271
Ocholla-T <tonnyocholla8@gmail.com>
2022-05-23 13:00:53 +0300

    ADD: forwardRef to card to enable use by projects component. STYLE: moved hover animation to be carried out by gsap to prevent conflicts with animations in pure CSS

--- src/components/layouts/Projects/Card/index.tsx
+++ src/components/layouts/Projects/Card/index.tsx
@@ -1,5 +1,6 @@
 /* Dependencies */
-import { ReactNode } from 'react'
+import { forwardRef, ForwardedRef, ReactNode, MutableRefObject } from 'react'
+import { gsap } from 'gsap'
 
 /* Images */
 import { ReactComponent as OpenLink } from '@images/open-outline.svg'
@@ -8,7 +9,7 @@ import { ReactComponent as GithubIcon } from '@images/logo-github.svg'
 /* Styling */
 import './styles.scss'
 
-interface Props {
+type Props = {
   children: ReactNode
   project: {
     githubLink: string
@@ -19,30 +20,54 @@ interface Props {
   }
 }
 
-function Card({ children, project }: Props): JSX.Element {
-  const goToProject = (): void => {
-    window.open(`${project.projectLink}`, '_blank')
-  }
+const Card = forwardRef(
+  ({ children, project }: Props, forwardedRef: ForwardedRef<HTMLElement | null>): JSX.Element => {
+    const goToProject = (): void => {
+      window.open(`${project.projectLink}`, '_blank')
+    }
 
-  return (
-    <article className="card flex flex-jc-sb" onClick={goToProject}>
-      <div className="card__text">
-        <p className="card__text--tech"> {project.techUsed.join(' ')}</p>
+    const scaleCard = (): void => {
+      gsap.to((forwardedRef as MutableRefObject<HTMLElement>)?.current, {
+        scale: 1.05,
+        duration: 0.3,
+        ease: 'linear.easeInOut',
+      })
+    }
 
-        <h2 className="card__text--title">{project.projectTitle}</h2>
-        <p className="card__text--description">{project.projectDescription}</p>
-        <div className="card__links flex">
-          <a href={project.projectLink} target="_blank" rel="noopener noreferrer">
-            <OpenLink className="card__links--icon card__links--icon-open" />
-          </a>
-          <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
-            <GithubIcon className="card__links--icon card__links--icon-github" />
-          </a>
+    const descaleCard = (): void => {
+      gsap.to((forwardedRef as MutableRefObject<HTMLElement>)?.current, {
+        scale: 1,
+        duration: 0.3,
+        ease: 'linear.easeInOut',
+      })
+    }
+
+    return (
+      <article
+        ref={forwardedRef}
+        className="card flex flex-jc-sb "
+        onClick={goToProject}
+        onMouseEnter={scaleCard}
+        onMouseLeave={descaleCard}
+      >
+        <div className="card__text">
+          <p className="card__text--tech"> {project.techUsed.join(' ')}</p>
+
+          <h2 className="card__text--title">{project.projectTitle}</h2>
+          <p className="card__text--description">{project.projectDescription}</p>
+          <div className="card__links flex">
+            <a href={project.projectLink} target="_blank" rel="noopener noreferrer">
+              <OpenLink className="card__links--icon card__links--icon-open" />
+            </a>
+            <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
+              <GithubIcon className="card__links--icon card__links--icon-github" />
+            </a>
+          </div>
         </div>
-      </div>
-      {children}
-    </article>
-  )
-}
+        {children}
+      </article>
+    )
+  },
+)
 
 export default Card
