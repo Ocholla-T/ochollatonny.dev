118bb86a8e3653bdbf665d841d876dabbd62a307
Ocholla-T <tonnyocholla8@gmail.com>
2022-05-23 12:54:11 +0300

    STYLING: added animation on entry to page

--- src/components/layouts/HeroSection/index.tsx
+++ src/components/layouts/HeroSection/index.tsx
@@ -1,38 +1,76 @@
+/**Dependencies */
+import { useRef, useEffect } from 'react'
+import { gsap } from 'gsap'
+
 /* Images */
-import { ReactComponent as OpenLink } from '@images/open-outline.svg';
-import { ReactComponent as ScrollIcon } from '@images/arrow-down-outline.svg';
-import Resume from '@images/Ocholla Tonny Resume.pdf';
+import { ReactComponent as OpenLink } from '@images/open-outline.svg'
+import { ReactComponent as ScrollIcon } from '@images/arrow-down-outline.svg'
+import Resume from '@images/Ocholla Tonny Resume.pdf'
 
 /* Components */
-import Button from '@components/Button';
+import Button from '@components/Button'
 
 /* Styling */
-import './styles.scss';
+import './styles.scss'
 
 interface HeroSectionProps {
-  isMobileMenuOpen: boolean;
+  isMobileMenuOpen: boolean
 }
 
 function HeroSection({ isMobileMenuOpen }: HeroSectionProps): JSX.Element {
+  const h1 = useRef<HTMLHeadingElement>(null)
+  const h4 = useRef<HTMLHeadingElement>(null)
+  const paragraphOne = useRef<HTMLParagraphElement>(null)
+  const paragraphTwo = useRef<HTMLParagraphElement>(null)
+  const heroButton = useRef<HTMLAnchorElement>(null)
+
+  const tl = gsap.timeline({
+    defaults: {
+      ease: 'Power4.inOut',
+      duration: 2,
+    },
+  })
+
+  useEffect(() => {
+    tl.to([h1.current, h4.current, paragraphOne.current, paragraphTwo.current], {
+      'clip-path': 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)',
+      opacity: 1,
+      y: 0,
+      duration: 2.2,
+    }).to(
+      heroButton.current,
+      {
+        'clip-path': 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)',
+        opacity: 1,
+        y: 0,
+      },
+      '-=2',
+    )
+  })
+
   return (
     <section className={`hero flex flex-fd-c ${isMobileMenuOpen && 'blur'} `}>
-      <h4>Hi there &#128075;, I'm</h4>
-      <h1>Ocholla Tonny.</h1>
-      <p>
+      <h4 ref={h4} className="hero__text">
+        Hi there &#128075;, I'm
+      </h4>
+      <h1 ref={h1} className="hero__text">
+        Ocholla Tonny.
+      </h1>
+      <p ref={paragraphOne} className="hero__text">
         <span>Frontend web developer</span> specializing in delivering digital experiences for the
         web.
       </p>
-      <p>
+      <p ref={paragraphTwo} className="hero__text">
         I'm always focused on continuous growth when learning & creating accessible, user-centred
         products.
       </p>
-      <Button buttonLink={Resume} className="hero__button">
+      <Button ref={heroButton} buttonLink={Resume} className="hero__button">
         Resume
         <OpenLink className="hero__button__icon" />
       </Button>
       <ScrollIcon className="scroll-icon hide-for-mobile" />
     </section>
-  );
+  )
 }
 
-export default HeroSection;
+export default HeroSection
